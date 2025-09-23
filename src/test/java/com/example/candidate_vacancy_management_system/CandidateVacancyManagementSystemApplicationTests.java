130760ddbb69d9ff2c78d21ced3d8595eb1695d9
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2025-09-23 09:39:46 +0700

    initialize spring project

---
+++ src/test/java/com/example/candidate_vacancy_management_system/CandidateVacancyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candidate_vacancy_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateVacancyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
